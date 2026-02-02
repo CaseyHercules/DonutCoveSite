55aed1ea63baaa4968d808fc6e5fa047aefebb42
Casey Hercules <Kchholman@gmail.com>
2026-02-02 14:39:18 -0600

    config issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import cloudflare from '@astrojs/cloudflare';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'server', // Enable server-side rendering for API routes
   adapter: cloudflare(), // Cloudflare Pages adapter
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      allowedHosts: ['prophet.local', '192.168.86.73'],
-    },
-  },
 });
